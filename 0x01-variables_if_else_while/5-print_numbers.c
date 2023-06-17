dca60a25658e54c0506cd2b226eccc9c848c7b1b
Senyo197 <senyo197@gmail.com>
2023-06-16 23:01:47 -0700

    single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - print all single digit numbers
+*Return: 0, unless conditioned
+*/
+int main(void)
+{
+int sdn = 0;
+for (sdn = 0; sdn <= 9; sdn++)
+{
+printf("%d", sdn);
+}
+printf("\n");
+
+return (0);
+}
