cc494f9c2c4ca50e56663d7a9d049348db234133
Senyo197 <senyo197@gmail.com>
2023-06-18 04:23:40 -0700

    printing error message

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - print error messages
+*Return: 1
+*/
+int main(void)
+{
+fprintf(stderr, "%s", "and that piece of art is useful");
+fprintf(stderr, "%s\n", "\" - Dora Korpar, 2015-10-19");
+
+return (1);
+}
