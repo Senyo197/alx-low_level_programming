23e021a7aabf6e4d31b142ab253bc7c5edced0da
Senyo197 <senyo197@gmail.com>
2023-06-18 05:15:22 -0700

    3 set combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+*main - prints combination of 3 different set of numbers
+*Return: 0 always, unless conditioned
+*/
+int main(void)
+{
+int fcn, scn, tcn;
+
+for (fcn = 0; fcn <= 9; fcn++)
+{
+for (scn = fcn + 1; scn <= 9; scn++)
+{
+for (tcn = scn + 1; tcn <= 9; tcn++)
+{
+putchar(fcn + '0');
+putchar(scn + '0');
+putchar(tcn + '0');
+if (fcn < 7 || scn < 8 || tcn < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
