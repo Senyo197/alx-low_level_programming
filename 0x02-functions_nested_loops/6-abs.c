41c93edc5859720f49e5b7c2e12af4dc8752aa67
Senyo197 <senyo197@gmail.com>
2023-06-21 15:27:59 -0700

    determine absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*_abs - determines the absolute value of a number
+*
+*@n: argument to check code
+*
+*Return: absolute value.
+*/
+
+int _abs(int n)
+{
+int absn = abs(n);
+return (absn);
+}
