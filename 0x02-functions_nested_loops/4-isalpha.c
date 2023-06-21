780d27ba8de4c56778902b4c25fb2904ea1f5c2c
Senyo197 <senyo197@gmail.com>
2023-06-21 13:04:38 -0700

    checking alphabetic characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_isalpha - checks alphabetic characters
+*
+*@c: checks for alphabetic characters
+*
+*Return: 1 for alphabetic charaters, 0 for other characters
+*/
+int _isalpha(int c)
+{
+	if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+		return 1;
+}
+	else
+{
+	return 0;
+}
+}
