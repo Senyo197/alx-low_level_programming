69c4e8bf1c6234c51e0ee6fda783f22bb116e544
Senyo197 <senyo197@gmail.com>
2023-06-21 09:22:12 -0700

    correction

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*main - prints putchar
+*Return: 0
+*/
+int main(void)
+{
+my_putchar("_")
+my_putchar("p");
+my_putchar("u");
+my_putchar("t");
+my_putchar("c");
+my_putchar("h");
+my_putchar("a");
+my_putchar("r");
+my_putchar("\n");
+return (0);
+}
