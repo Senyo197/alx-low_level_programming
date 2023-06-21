62b52075b6d11dc39e508b87803c450b9ac39b37
Senyo197 <senyo197@gmail.com>
2023-06-21 11:18:50 -0700

    corrected the conditions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,16 @@
 */
 void print_alphabet_x10(void)
 {
-int lwcx10 = 'a';
+char lwcx10 = 'a';
+int increment = 0;
 
-for (lwcx10 = 'a'; lwcx10 >= 'z'; lwcx10++)
+for (lwcx10 = 'a'; lwcx10 <= 'z'; lwcx10++)
 {
-	while (lwcx10 >= 10)
+	while (lwcx10 < 10)
 {
 	_putchar(lwcx10);
 }
 	_putchar('\n');
+	increment++;
 }
 }
-
