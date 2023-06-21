2f985b2d1886b0d40bf7e8eea6e6d896282414df
Senyo197 <senyo197@gmail.com>
2023-06-21 10:01:15 -0700

    prints lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*print_alphabet - prints lowercase alphabets
+*void
+*/
+void print_alphabet(void)
+{
+	char lwc = 'a';
+
+	for (lwc = 'a'; lwc <= 'z'; lwc++)
+		_putchar(lwc);
+}
+
+/**
+*main - chack the code
+*Return: 0 always, unless conditioned
+*/
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
