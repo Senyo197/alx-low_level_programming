645bde3af01d00eceeb78fc84fbfc03df2fcc527
Senyo197 <senyo197@gmail.com>
2023-06-22 07:31:22 -0700

    removed spaces in code

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+/**
+ *main - this function tells whether or not number is - || + || 0
+*Return: 0, unless conditioned
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else
+{
+printf("%d is zero\n", n);
+}
+
+return (0);
+}
