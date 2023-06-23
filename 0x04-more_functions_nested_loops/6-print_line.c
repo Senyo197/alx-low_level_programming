47d9c12388da3e3bbf089d619dda6a35d7e53d49
Senyo197 <senyo197@gmail.com>
2023-06-23 15:26:41 -0700

    print straigt lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,23 @@
 #include "main.h"
 
-
+/**
+*print_line - prints straight line in the terminal
+*@n: used to checked the code
+*/
 void print_line(int n)
 {
-int strtl
+int strt_ln;
+
+if (n <= 0)
+{
+	_putchar('\n');
+}
+else
+{
+	for (strt_ln = 0; strt_ln < n; strt_ln++)
+{
+	_putchar('_');
+}
+_putchar('\n');
+}
 }
