7c52e5c5e2e849aa242358d803da216cf4a69388
Senyo197 <senyo197@gmail.com>
2023-06-24 13:50:25 -0700

    print integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_number - is a function to print all integers
+*@n: is integer
+*/
+void print_number(int n)
+{
+int itg = n;
+
+	if (itg < 0)
+{
+	itg = -itg;
+	_putchar(45); /* ASCII "-" */
+}
+	else if (itg > 9)
+{
+	print_number(itg / 10);
+}
+	_putchar((itg % 10) + '0');
+}
