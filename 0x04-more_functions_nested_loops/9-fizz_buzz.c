acee0624d9ba588e57067ac9feec9516efee955c
Senyo197 <senyo197@gmail.com>
2023-06-23 17:33:49 -0700

    prints numbers from 1 tp 100

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*main - prints number from 1 to 100
+*Return: 0
+*/
+int main(void)
+{
+int numb = 1;
+
+while (numb <= 100)
+{
+	if (numb % 3 == 0)
+{
+	printf("Fizz");
+}
+	else if (numb % 5 == 0)
+{
+	printf("Buzz");
+}
+	else if ((numb % 3 == 0) && (numb % 5 == 0))
+{
+	printf("FizzBuzz");
+}
+	else
+{
+	printf("%d", numb);
+}
+	numb++;
+	if (numb != 101)
+{
+	printf(" ");
+}
+}
+printf("\n");
+
+return (0);
+}
