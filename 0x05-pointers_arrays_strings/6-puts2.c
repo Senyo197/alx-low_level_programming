38ec32e8348e07e15694457c976241931582fbc8
Senyo197 <senyo197@gmail.com>
2023-06-26 15:22:08 -0700

    skip characters by 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,10 +5,13 @@
 */
 void puts2(char *str)
 {
-int a;
-for (a = 0; str[a] != '\0'; a += 2)
+int a, b;
+for (a = 0; str[a] != '\0'; a++)
 {
-	_putchar(str[a]);
+}
+for (b = 0; b < a; b += 2)
+{
+	_putchar(str[b]);
 }
 _putchar('\n');
 }
