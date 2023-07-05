029d9d5b4e487091468641c3017faea1759bd1e1
Senyo197 <senyo197@gmail.com>
2023-07-05 14:19:39 -0700

    powers of number

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,26 +1,25 @@
 #include "main.h"
 
 /**
-  *_pow_recursion - finds power of a number
+  *_pow_recursion - calculates powers of numbers
   *@x: number to be exponented
-  *@y: exponetial number
-  *Return: -1 if number is < 0, 1 if 0, else factorial of numbers
+  *@y: exponential number
+  *Return: powers of number, -1 if < 0 and 1 if = 0
   */
 int _pow_recursion(int x, int y)
 {
-        int rst;
+int rst;
 
-        if (y < 0)
-        {
-                return (-1);
-        }
+if (y < 0)
+{
+	return (-1);
+}
 
-        if (y == 0)
-        {
-                return (1);
-        }
+if (y == 0)
+{
+	return (1);
+}
 
-        rst = x * _pow_recursion(x, (y - 1));
-
-        return (rst);
+rst = x * _pow_recursion(x, (y - 1));
+return (rst);
 }
