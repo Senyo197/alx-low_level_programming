7cf2a5b274e325a79aacbc859be1d9e39953c9a0
Senyo197 <Senyo197@gmail.com>
2023-07-10 13:53:43 -0700

    dupliacte string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  *_strdup - function to duplicate string
+  *@str: string to be duplicated
+  *Return: duplicated string
+  */
+char *_strdup(char *str)
+{
+	size_t lnth = strlen(str) + 1;
+
+	char *dupr = (char *)malloc(lnth);
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+
+	if (dupr != NULL)
+	{
+		memcpy(dupr, str, lnth);
+	}
+
+	return (dupr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,6 +3,9 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
+
+char *_strdup(char *str);
 
 char *create_array(unsigned int size, char c);
 
