9ee4a4ce53bcfd02ab4e65014c820762c872dd35
Senyo197 <Senyo197@gmail.com>
2023-07-10 12:16:49 -0700

    create array of characters

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  *create_array - create array of characters
+  *@size: array size
+  *@c: characters
+  *Return: array
+  */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int a;
+	char *arr = (char *)malloc(sizeof(char) * (size + 1));
+
+	if (arr != NULL)
+	{
+		for (a = 0; a < size; a++)
+		{
+			arr[a] = c;
+		}
+
+		arr[size] = '\0';
+	}
+
+	return (arr);
+}
