8545a6423edf09ea2a0315294dbcb47d08eed908
Senyo197 <Senyo197@gmail.com>
2023-07-11 13:17:40 -0700

    101

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *strtow - split strings
+  *@str: strings
+  *Return: null
+  */
+char **strtow(char *str)
+{
+
+	if (str == NULL || *str == '\0')
+	{
+		return (NULL);
+	}
+
+	return (1);
+}
