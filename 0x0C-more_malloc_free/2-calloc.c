c49aa2c0eaa91fe0e760ffb83f8baed69873bb52
Senyo197 <Senyo197@gmail.com>
2023-07-13 13:34:57 -0700

    allocate memory with malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  *_calloc - allocate memory
+  *@nmemb: array
+  *@size: data type size
+  *Return: allocated memmory or nothing
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *value;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	value = malloc(nmemb * sizeof(size));
+
+	return (value);
+}
