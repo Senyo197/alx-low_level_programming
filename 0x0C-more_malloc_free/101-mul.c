f082a9f8446ab21b7c04feadb5aededb5c2594af
Senyo197 <Senyo197@gmail.com>
2023-07-14 16:51:49 -0700

    multiply two numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,56 @@
 #include "main.h"
 
-
-void mul_2_numbers(int numb)
+/**
+  *check_numbers - checks if character is a number
+  *@numb: character to be checked
+  *Return: 1 if there's a number, 0 for not a number
+  */
+int check_numbers(char *numb)
 {
+	int a;
 
+	for (a = 0; numb[a] != '\0'; a++)
+	{
+		if (!isdigit(numb[a]))
+		{
+			return (0);
+		}
+	}
+
+	return (1);
+}
+
+/**
+*mul - multiply two numbers
+*@num1: first number
+*@num2: second number
+*/
+void mul(char *num1, char *num2)
+{
+	size_t rst = strtoull(num1, NULL, 10) *
+		strtoull(num2, NULL, 10);
+	printf("%lu\n", (unsigned long)rst);
+}
+
+/**
+  *main - prints multiplication of two numbers
+  *@argc: number of command line arguments
+  *@argv: array of command line arguments
+  *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		return (98);
+	}
+
+	if (!check_numbers(argv[1]) || !check_numbers(argv[2]))
+	{
+		return (98);
+	}
+
+	mul(argv[1], argv[2]);
+
+	return (0);
 }
