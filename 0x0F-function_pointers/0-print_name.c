25ddbec6973137a2a5ce0f80a7fec1277a6354ed
Senyo197 <Senyo197@gmail.com>
2023-07-19 14:52:02 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+  *print_name - function to print name
+  *@name: name to be printed
+  *@f: pointer function
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL || name == NULL)
+	{
+		return;
+	}
+
+	(*f)(name);
+}
