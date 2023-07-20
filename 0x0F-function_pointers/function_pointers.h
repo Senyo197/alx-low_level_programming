1e2861ce4db9c53417a6c8fffbf85dd5c2f3cfd5
Senyo197 <Senyo197@gmail.com>
2023-07-20 00:22:56 -0700

    print operation code

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,7 +7,7 @@
 void print_putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
-int (int argc, char *argv[]);
+int main(int argc, char *argv[]);
 void prnt_opcode(int number_of_bytes);
 
 void array_iterator(int *array, size_t size, void (*action)(int));
