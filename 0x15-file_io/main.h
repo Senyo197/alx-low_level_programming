8249f03fd11f5307dc39a661dd63551158a1784b
Senyo197 <Senyo197@gmail.com>
2023-08-12 09:40:55 -0700

    Fixed the conflicting function prototypes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-void print_error(const char *message);
+void print_error(int code, const char *message, const char *file);
 void print_class(unsigned char elf_class);
 void print_data(unsigned char data_encoding);
 void print_version(Elf32_Word version);
