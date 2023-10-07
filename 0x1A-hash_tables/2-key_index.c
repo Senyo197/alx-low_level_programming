8c22f671ebc1b51ecd4ce317571510931e03edce
Senyo197 <Senyo197@gmail.com>
2023-10-07 09:47:22 +0000

    Determinig the index of a key

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+*Determines the index of key
+*@key: The Key's index to determined
+*@size: size of the values
+*Return: The index of the key
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash_value = hash_djb2(key);
+	unsigned long int index = hash_value % size;
+
+	return (index);
+}
