f57b4cf55b22dc751fe80829ee5886b1b6928f09
Senyo197 <Senyo197@gmail.com>
2023-10-07 08:37:07 +0000

    error corrected

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int a;
 
 	hash_table_t *new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+
 	if (new_table == NULL)
-    {
+	{
 		return (NULL);
 	}
 
